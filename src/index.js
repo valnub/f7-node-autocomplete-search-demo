b2854423effc60ec905a1dfd3e110983a22bad93
Timo <mail@timo-ernst.net>
2018-11-02 13:52:24 +0100

    Initial commit with F7 react template

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// Import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import Framework7
+import Framework7 from 'framework7/framework7.esm.bundle';
+
+// Import Framework7-React plugin
+import Framework7React from 'framework7-react';
+
+// Import main App component
+import App from './components/App.jsx';
+
+// Framework7 styles
+import 'framework7/css/framework7.min.css';
+
+// Icons
+import './css/icons.css';
+
+// Custom app styles
+import './css/app.css';
+
+// Init Framework7-React plugin
+Framework7.use(Framework7React);
+
+// Mount React App
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById('app'),
+);
